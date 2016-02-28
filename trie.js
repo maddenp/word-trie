95a9c122e93aeb9806258e780ff4ba37a39eaa0d
Paul Madden <maddenp@colorado.edu>
2016-02-28 11:58:42 -0700

    Remove str_ prefix from function names

--- trie.js
+++ trie.js
@@ -3,12 +3,12 @@
 
 "use strict";
 
-function str_trie_insert(trie, word) {
+function trie_insert(trie, word) {
   word.split('').forEach(c => { trie = (trie[c] = trie[c] || {}); });
   trie['.'] = true;
 }
 
-function str_trie_lookup(trie, word) {
+function trie_lookup(trie, word) {
   word.split('').forEach(c => { if (!(trie = trie[c])) return false; });
   return trie['.'];
 }
@@ -20,12 +20,12 @@ var reader = readline.createInterface({input: fs.createReadStream(filename)});
 var trie = {};
 
 reader.on('line', function (word) {
-  str_trie_insert(trie, word);
+  trie_insert(trie, word);
 });
 
 reader.on('close', function () {
   console.log(JSON.stringify(trie, null, 2));
-  console.log('apple: ' + str_trie_lookup(trie, 'apple'));
-  console.log('apples: ' + str_trie_lookup(trie, 'apples'));
-  console.log('bana: ' + str_trie_lookup(trie, 'bana'));
+  console.log('apple: ' + trie_lookup(trie, 'apple'));
+  console.log('apples: ' + trie_lookup(trie, 'apples'));
+  console.log('bana: ' + trie_lookup(trie, 'bana'));
 });

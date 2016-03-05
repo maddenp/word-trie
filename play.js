f02fdee37339d7295218cad270394dee1c2e3158
Paul Madden <maddenp@colorado.edu>
2016-03-05 13:15:28 -0700

    Simple command-line word-check loop

--- play.js
+++ play.js
@@ -4,9 +4,18 @@
 "use strict";
 
 const fs = require('fs');
+const lookup = require('./trie').lookup;
 
 const readfile = infile => {
   return JSON.parse(fs.readFileSync(infile, 'utf8'));
 };
 
-console.log(readfile('trie.json'));
+const prompt = () => process.stdout.write('> ');
+const trie = readfile('trie.json');
+
+require('readline').createInterface({input: process.stdin}).on('line', word => {
+  console.log(lookup(trie, word));
+  prompt();
+});
+
+prompt();
